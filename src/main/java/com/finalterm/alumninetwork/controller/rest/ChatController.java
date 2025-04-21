049e4b87274259d39c759669efa0c0f647e9ff5b
Nguyen Le Anh Vu <vunguyen.0208200402@gmail.com>
2025-04-21 22:49:14 +0700

    fix

---
+++ src/main/java/com/finalterm/alumninetwork/controller/rest/ChatController.java
@@ -0,0 +1,25 @@
+package com.finalterm.alumninetwork.controller.rest;
+
+import com.finalterm.alumninetwork.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController {
+    @Autowired
+    private ChatService chatService;
+
+    @PostMapping
+    public ResponseEntity<String> ask(@RequestBody Map<String, String> req) {
+        String userInput = req.get("question");
+        String reply = chatService.chat(userInput);
+        return ResponseEntity.ok(reply);
+    }
+}
